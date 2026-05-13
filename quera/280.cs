611ef1d5c748d6cfed043878a2343812e45ec637
Sadra Adibi <sadraadibi@gmail.com>
2026-05-13 11:07:27 +0330

    280

--- quera/280.cs
+++ quera/280.cs
@@ -18,8 +18,8 @@ namespace quera
 
             if (
                 a * a == (b * b) + (c * c) ||
-                b*b == (a * a) + (c * c) ||
-                c*c == (a * a) + (b * b)
+                b * b == (a * a) + (c * c) ||
+                c * c == (a * a) + (b * b)
                 )
             {
                 Console.WriteLine("YES");
