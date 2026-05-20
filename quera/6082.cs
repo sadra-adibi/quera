ad383b72a3b0c34ba0c6460ee83bbcfdf3b3611e
Sadra Adibi <sadraadibi@gmail.com>
2026-05-20 17:31:42 +0330

    6082

---
+++ quera/6082.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+
+                for (int j = 0; j < m; j++)
+                {
+                    if (line[j] == '*')
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
+    }
+}
+/*
+https://quera.org/problemset/6082
+*/
