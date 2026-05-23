531277ca3926cc54b3b808b4399dd734b19cb760
Sadra Adibi <sadraadibi@gmail.com>
2026-05-23 13:54:36 +0330

    2637 hotfix

---
+++ quera/2637.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            int input = int.Parse(Console.ReadLine());
+            int ofogi = input / 2;
+            int amodi = input - ofogi;
+            int max_parts = (ofogi + 1) * (amodi + 1);
+            Console.WriteLine(max_parts);
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
+    }
+}
+/*
+https://quera.org/problemset/2637
+*/
